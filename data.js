5ac2f171964ee699bf9c64c60692bed08d28e1a1
MANIVENKATRAJ M <127532509+mp143mani@users.noreply.github.com>
2023-08-26 10:39:11 +0530

    ss

---
+++ data.js
@@ -0,0 +1,125 @@
+const data = [
+    {
+      "id": "1",
+      "name": "Adhithya Senger",
+      "email": "Adhithya_Xander_Collier@yahoo.com"
+    },
+    {
+      "id": "2",
+      "name": "Akila Weber",
+      "email": "Akila_Bennett_Kreiger11@yahoo.com"
+    },
+    {
+      "id": "3",
+      "name": "Aravind Hahn",
+      "email": "Aravind_Juston.Altenwerth@yahoo.com"
+    },
+    {
+      "id": "4",
+      "name": "Pavithra Bailey",
+      "email": "Pavithra_Guiseppe_Hegmann@yahoo.com"
+    },
+    {
+      "id": "5",
+      "name": "Prakash Keeling",
+      "email": "Prakash_Louisa_Walsh18@hotmail.com"
+    },
+    {
+      "id": "6",
+      "name": "Chandra Crona",
+      "email": "Chandra_Chandler_Abernathy@yahoo.com"
+    },
+    {
+    "id": "11",
+      "name": "Kural Rau",
+      "email": "Kural_Orin45@gmail.com"
+    },
+    {
+      "id": "12",
+      "name": "Harish Gutmann MD",
+      "email": "Harish_Alyce.Stracke37@yahoo.com"
+    },
+    {
+      "id": "13",
+      "name": "Indira Torphy",
+      "email": "Indira_Dean_Breitenberg71@hotmail.com"
+    },
+    {
+      "id": "14",
+      "name": "Jothi Lang",
+      "email": "Jothi_Tatum.Ullrich@yahoo.com"
+    },
+    {
+      "id": "15",
+      "name": "Krishna Green DDS",
+      "email": "Krishna_Dino83@gmail.com"
+    },
+    {
+      "id": "16",
+      "name": "Lakshmi Gerhold",
+      "email": "Lakshmi_Lilian_Bashirian8@hotmail.com"
+    },
+    {
+        "id": "17",
+        "name": "Adhithya Senger",
+        "email": "Adhithya_Xander_Collier@yahoo.com"
+      },
+      {
+        "id": "18",
+        "name": "Akila Weber",
+        "email": "Akila_Bennett_Kreiger11@yahoo.com"
+      },
+      {
+        "id": "19",
+        "name": "Aravind Hahn",
+        "email": "Aravind_Juston.Altenwerth@yahoo.com"
+      },
+      {
+        "id": "20",
+        "name": "Pavithra Bailey",
+        "email": "Pavithra_Guiseppe_Hegmann@yahoo.com"
+      },
+      {
+        "id": "21",
+        "name": "Prakash Keeling",
+        "email": "Prakash_Louisa_Walsh18@hotmail.com"
+      },
+      {
+        "id": "22",
+        "name": "Chandra Crona",
+        "email": "Chandra_Chandler_Abernathy@yahoo.com"
+      },
+      {
+      "id": "22",
+        "name": "Kural Rau",
+        "email": "Kural_Orin45@gmail.com"
+      },
+      {
+        "id": "24",
+        "name": "Harish Gutmann MD",
+        "email": "Harish_Alyce.Stracke37@yahoo.com"
+      },
+      {
+        "id": "25",
+        "name": "Indira Torphy",
+        "email": "Indira_Dean_Breitenberg71@hotmail.com"
+      },
+      {
+        "id": "26",
+        "name": "Jothi Lang",
+        "email": "Jothi_Tatum.Ullrich@yahoo.com"
+      },
+      {
+        "id": "27",
+        "name": "Krishna Green DDS",
+        "email": "Krishna_Dino83@gmail.com"
+      },
+      {
+        "id": "28",
+        "name": "Lakshmi Gerhold",
+        "email": "Lakshmi_Lilian_Bashirian8@hotmail.com"
+      },
+    
+];
+
+export {data};
